ddb26cfe66b26e8a934955e4bb63bc9c2eaef27e
Schlabaga <78259357+Schlabaga@users.noreply.github.com>
2023-04-07 18:31:32 +0200

    Résolu pb d'auth (getenv etc)

--- config.py
+++ config.py
@@ -2,9 +2,13 @@ import pymongo
 from discord.ext import commands
 import discord
 import os
+from dotenv import load_dotenv
 
-MONGO_CLIENT_KEY = os.environ.get('MONGO_CLIENT_KEY')
-TOKEN = os.environ.get('TOKEN')
+load_dotenv()
+
+
+MONGO_CLIENT_KEY = os.getenv('MONGO_CLIENT_KEY')
+TOKEN = os.getenv('TOKEN')
 
 MongoClient = pymongo.MongoClient(MONGO_CLIENT_KEY)
 dbUser = MongoClient.userconfig
