e7100bbcac3ca5ce314135e6fa0d46cf73d1876a
Schlabaga <78259357+Schlabaga@users.noreply.github.com>
2024-07-16 17:49:43 +0200

    Ajout cdn

--- main.py
+++ main.py
@@ -766,9 +766,9 @@ async def upload_crosshairs(interaction: discord.Interaction, type: discord.app_
         for root, dirs, files in os.walk(dossier):
             crosshair_id = root.split("\\")[-1]
             
-            crosshair_data = dbValorant.crosshairs.find_one({"id": crosshair_id})
+            crosshair_data = dbValorant.crosshairs.find_one({"id": crosshair_id, "isSpecialUploaded": {"$exists": False}})
             try:
-                if crosshair_data["blank"].startswith("https://cdn.") or crosshair_data["isSpecialUploaded"] == True:
+                if crosshair_data["blank"].startswith("https://cdn."):
                     print("Already uploaded")
                     dbValorant.crosshairs.update_one({"id": crosshair_id}, {"$set":{"isSpecialUploaded":True}}, upsert=True)
                     continue
